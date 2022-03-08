906702ce6e2509cb22f99896daece028b8b6caac
kancheng <zxdfgcv@gmail.com>
2022-03-08 18:39:37 +0800

    Added Vue labs

---
+++ DIP/tech/vue-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorldTest msg="OwO /// Testing Kan !!"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HelloWorldTest from './components/HelloWorldTest.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, HelloWorldTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
